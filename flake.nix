e6f29c13446bdbc396610101514f0bb1c89e386d
Axel Denis <axel.denis@epitech.eu>
2025-05-31 15:00:41 +0900

    feat: import original source code

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.cmake
+            pkgs.vcpkg
+            pkgs.gcc
+            pkgs.pkg-config
+          ];
+        };
+      });
+}
